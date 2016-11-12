81b2f7681ee8821b97c4343d6b5686ef9c2f9b37
Ekke Kaha <Ekke Kaha>
2016-11-12 19:01:20 +0200

    floorgit add -A!

--- map.py
+++ map.py
@@ -3,11 +3,14 @@ import constants
 
 class LowerPlatform():
     def __init__(self, x, y, w, h):
+        self.x = x -25
+        self.y = y
         self.rect = pygame.Rect([x, y, w, h])
         self.color = [255, 0, 0]
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+        self.rectangle = pygame.Rect([self.x, self.y, 0, 0])
+        self.blit = pygame.image.load("art_assets/lava_alumine.png")
+    def draw(self, s):
+        s.blit(self.blit, self.rectangle)
 
 
 class UpperPlatform():
