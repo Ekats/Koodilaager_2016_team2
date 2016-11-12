639ac33e9ee39eca1cac3d0cf295782cadaa4dd5
Ekke Kaha <Ekke Kaha>
2016-11-12 14:01:09 +0200

    Modified sound files

--- audio.py
+++ audio.py
@@ -2,4 +2,9 @@ import pygame
 
 class Audio():
     def __init__(self):
-        
+        pygame.mixer.pre_init(44100, 16, 2, 4096)
+        self.bg_sound = pygame.mixer.music.load("audiofiles/soundtrack.ogg")
+
+    def play_background(self):
+        pygame.mixer.music.play()
+        # self.bg_sound.play(-1)
