fc69fcf7570f839c075bc9c1bb57cbf459aafcfc
Ekke Kaha <Ekke Kaha>
2016-11-12 19:30:24 +0200

    upperstage textures

--- map.py
+++ map.py
@@ -16,11 +16,11 @@ class LowerPlatform():
 
 class UpperPlatform():
     def __init__(self, x, y, w, h):
-        self.x = x -25
+        self.x = x
         self.y = y
         self.rect = pygame.Rect([x, y, w, h])
         self.color = [255, 0, 0]
-        self.blit = pygame.image.load("art_assets/lava_ylemine.png")
+        self.blit = pygame.image.load("art_assets/lava_ylemine - Copy.png")
         self.rectangle = pygame.Rect([self.x, self.y, 0, 0])
     def draw(self, s):
         s.blit(self.blit, self.rectangle)
