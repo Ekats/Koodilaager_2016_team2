318e48c3ea3d901a264b35ef3b5dd6e7004546c6
Ekke Kaha <Ekke Kaha>
2016-11-12 15:59:33 +0200

    added def-s to audio.py

--- audio.py
+++ audio.py
@@ -8,13 +8,34 @@ class Audio():
         pygame.mixer.pre_init(44100, 16, 2, 4096)
         self.bg_sound = pygame.mixer.music.load("audiofiles/soundtrack.ogg")
 
-    def play_background(self):
+    def play_background(self):  #Background music
         pygame.mixer.music.play(-1)
         # self.bg_sound.play(-1)
-    def pussy(self):
+    def pussy(self):    #Grab em by the pussy sound
         a = pygame.mixer.Sound("audiofiles/pussygrab.ogg")
         pygame.mixer.Sound.play(a)
-    def trumpwin(self):
+    def hit(self):  #Hit sounds
+        a = pygame.mixer.Sound("audiofiles/hit0.ogg")
+        b = pygame.mixer.Sound("audiofiles/hit1.ogg")
+        c = pygame.mixer.Sound("audiofiles/hit2.ogg")
+        d = pygame.mixer.Sound("audiofiles/hit3.ogg")
+        e = pygame.mixer.Sound("audiofiles/hit4.ogg")
+
+        if chance == 0:
+            pygame.mixer.Sound.play(a)
+        elif chance == 1:
+            pygame.mixer.Sound.play(b)
+        elif chance == 2:
+            pygame.mixer.Sound.play(c)
+        elif chance == 3:
+            pygame.mixer.Sound.play(d)
+        elif chance == 4:
+            pygame.mixer.Sound.play(e)
+    def jump(self): #Jump sound
+        a = pygame.mixer.Sound("audiofiles/jump.ogg")
+        pygame.mixer.Sound.play(a)
+
+    def trumpwin(self): #Trump win sound
         chance = randint(0, 2)
 
         a = pygame.mixer.Sound("audiofiles/trumpwin0.ogg")
@@ -27,7 +48,7 @@ class Audio():
             pygame.mixer.Sound.play(b)
         elif chance == 2:
             pygame.mixer.Sound.play(c)
-    def clintonwin(self):
+    def clintonwin(self):   #Clinton win sound
         chance = randint(0, 2)
 
         a = pygame.mixer.Sound("audiofiles/clintonwin0.ogg")
