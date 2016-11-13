483100fab688784bf7de0de6fa72617181be9637
Progeja <Progeja>
2016-11-13 06:08:41 +0200

    elud

--- constants.py
+++ constants.py
@@ -3,3 +3,6 @@ import pygame
 RESOLUTION = [800, 450]
 stage_lower = pygame.image.load("lava_alumine.png")
 stage_upper = pygame.image.load("lava_ylemine.png")
+banner_l = pygame.image.load("art_assets/ylribaL.png")
+banner_r = pygame.image.load("art_assets/ylribaP.png")
+life = pygame.image.load("art_assets/hearth.png")
